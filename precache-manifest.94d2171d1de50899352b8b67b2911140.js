5e05446d74b0102cfad64df9ac175b3cb6f1da1e
Adrian Gonzalo <gonzahh@gmail.com>
2019-06-07 03:18:05 +0200

    Updates

---
+++ precache-manifest.94d2171d1de50899352b8b67b2911140.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8af2ef6b9c34c712c6c3e75d949204",
+    "url": "/remoteok-react/index.html"
+  },
+  {
+    "revision": "c844ed15c65402e97b79",
+    "url": "/remoteok-react/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "fc5c42adc9f1d1d2304e",
+    "url": "/remoteok-react/static/js/2.8ec4bcaf.chunk.js"
+  },
+  {
+    "revision": "c844ed15c65402e97b79",
+    "url": "/remoteok-react/static/js/main.9aac9df3.chunk.js"
+  },
+  {
+    "revision": "e33d34989bc86498e4e2",
+    "url": "/remoteok-react/static/js/runtime~main.7805b21e.js"
+  }
+]);
